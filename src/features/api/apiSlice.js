f22f6fe64c254ce9c232ca247ae69de9471735f2
GUchechi <godswilluchendu7@gmail.com>
2023-05-08 16:49:26 +0100

    apiSlice

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"],
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/todos",
+    }),
+  }),
+});
+
+
+export const {
+    useGetTodosQuery,
+    // useAddTodoMutation,
+    // useUpdateTodoMutation,
+    // useDeleteTodoMutation
+} = apiSlice
